7eee85f7d00f96a11accf8575ef3eabacd370e3e
Clem Freeman <clemfree@gmail.com>
2017-08-21 18:30:40 -0700

    Add react-map-gl

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import ReactMapGL from 'react-map-gl';
 import logo from './logo.svg';
 import './App.css';
+
+const token = process.env.REACT_APP_MAPBOX_TOKEN;
+console.log(token)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ReactMapGL
+        mapboxApiAccessToken={token}
+        width={window.innerWidth}
+        height={window.innerHeight}
+        latitude={45.5433229}
+        longitude={-122.794504}
+        zoom={10}
+        onViewportChange={(viewport) => {
+          const { width, height, latitude, longitude, zoom } = viewport;
+          // Optionally call `setState` and use the state to update the map.
+        }}
+      />
     );
   }
 }
