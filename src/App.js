1157a737e2acf281c7ba49497d822ce9d8f404fb
Clem Freeman <clemfree@gmail.com>
2017-08-22 08:06:07 -0700

    Add mapboxDark style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import ReactMapGL from 'react-map-gl';
-import logo from './logo.svg';
 import './App.css';
+import MapStyle from './map-style';
 
 const token = process.env.REACT_APP_MAPBOX_TOKEN;
-console.log(token)
 
 class App extends Component {
   render() {
     return (
       <ReactMapGL
         mapboxApiAccessToken={token}
+        mapStyle={MapStyle}
         width={window.innerWidth}
         height={window.innerHeight}
         latitude={45.5433229}
         longitude={-122.794504}
         zoom={10}
         onViewportChange={(viewport) => {
-          const { width, height, latitude, longitude, zoom } = viewport;
+          // const { width, height, latitude, longitude, zoom } = viewport;
           // Optionally call `setState` and use the state to update the map.
         }}
       />
