0d8caef563767a4f00a6d09fa945f014d9cddcc5
Clem Freeman <clemfree@gmail.com>
2017-08-30 15:12:51 -0700

    Add color stops to location fill

--- src/map-style.js
+++ src/map-style.js
@@ -14,7 +14,16 @@ style.layers.push({
   source: 'sleepingBoundaries',
   type: 'fill',
   paint: {
-    'fill-color': "#00ffff",
+    'fill-color': {
+      property: 'count',
+      stops: [
+        [50, '#edf8fb'],
+        [100, '#b2e2e2'],
+        [200, '#66c2a4'],
+        [300, '#2ca25f'],
+        [400, '#006d2c'],
+      ]
+    },
     'fill-outline-color': 'black',
     'fill-opacity': 0.5,
   },
