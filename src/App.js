3d1caed1f4f23f32ed9c840a9b7c8913f61e9889
Clem Freeman <clemfree@gmail.com>
2017-08-30 14:50:36 -0700

    Update counts in features onclick

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactMapGL from 'react-map-gl';
-import { Panel } from 'react-bootstrap';
+import { Panel, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import MapStyle from './map-style';
@@ -12,6 +12,7 @@ class App extends Component {
   state = {
     mapStyle: MapStyle,
     counts: {},
+    active: null,
   };
 
   componentDidMount() {
@@ -20,12 +21,48 @@ class App extends Component {
     }))
   }
 
+  onClick = year =>
+    () => {
+      this.setState({
+        active: year,
+      });
+      this.updateStyle();
+    }
+
+  updateStyle = () => {
+    if (this.state.active) {
+      const features = this.state.mapStyle
+        .getIn(['sources', 'sleepingBoundaries', 'data', 'features'])
+        .map(feature => {
+          return feature.setIn(
+            ['properties', 'count'],
+            this.state.counts[feature.getIn(['properties', 'name'])][this.state.active]
+          );
+        });
+      const newStyle = this.state.mapStyle
+        .setIn(['sources', 'sleepingBoundaries', 'data', 'features'], features);
+      this.setState({
+        mapStyle: newStyle,
+      });
+    }
+  }
+
   render() {
     return (
       <div>
-          <Panel className="controls" header="PIT Sleeping Locations">
-            Yo
-          </Panel>
+          { Object.keys(this.state.counts).length ?
+            <Panel className="controls" header="PIT Report Sleeping Locations">
+              <Button active={this.state.active === 2011} onClick={this.onClick(2011)}>
+                January 26th, 2011
+              </Button>
+              <Button active={this.state.active === 2013} onClick={this.onClick(2013)}>
+                January 30th, 2013
+              </Button>
+              <Button active={this.state.active === 2015} onClick={this.onClick(2015)}>
+                January 28th, 2015
+              </Button>
+            </Panel>
+            : null }
           <ReactMapGL
             mapboxApiAccessToken={token}
             mapStyle={this.state.mapStyle}
